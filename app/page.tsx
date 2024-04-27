ee96c293c4ac2074609fe7346e165a21ed290cc1
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2024-04-27 14:54:39 -0500

    fix link

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <h1 className={styles.nameIntro}>Bradley Schoeneweis</h1>
-      <p className={styles.bioParagraph}>I&apos;m a senior engineer at <InlineLink href='https://radar.com'>Radar</InlineLink> working remotely in <InlineLink href='https://radar.com/demo/maps#13.45/-97.33066148935688/32.75466640633965'>Fort Worth, TX</InlineLink>.</p>
+      <p className={styles.bioParagraph}>I&apos;m a senior engineer at <InlineLink href='https://radar.com'>Radar</InlineLink> working remotely in <InlineLink href='https://radar.com/demo/maps#14/32.75544435227022/-97.33047972836872'>Fort Worth, TX</InlineLink>.</p>
       <p className={styles.bioParagraph}>This is my medium for occasional writing and references to things I find interesting.</p>
       <div className={styles.linkSection}>
         <ArrowLink href='https://www.linkedin.com/in/bradley-schoeneweis' isExternalLink>linkedin</ArrowLink>
