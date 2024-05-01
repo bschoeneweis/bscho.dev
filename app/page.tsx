3774225f9af69b97897239a58b3f2a60290f3c2f
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2024-04-30 20:11:08 -0500

    no scroll on mobile for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <>
-      <h1 className={styles.nameIntro}>Bradley Schoeneweis</h1>
+      <h1 className={`${styles.nameIntro} homePageHeader`}>Bradley Schoeneweis</h1>
       <p className={styles.bioParagraph}>I&apos;m a senior engineer at <InlineLink href='https://radar.com'>Radar</InlineLink> working remotely in <InlineLink href='https://radar.com/demo/maps#14/32.75544435227022/-97.33047972836872'>Fort Worth, TX</InlineLink>.</p>
       <p className={styles.bioParagraph}>This is my medium for occasional writing and references to things I find interesting.</p>
       <div className={styles.linkSection}>
