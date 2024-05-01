a5dc4f734b79a29d011868e3a6184792df338c7a
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2024-04-30 20:46:56 -0500

    update map spot

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <h1 className={`${styles.nameIntro} homePageHeader`}>Bradley Schoeneweis</h1>
-      <p className={styles.bioParagraph}>I&apos;m a senior engineer at <InlineLink href='https://radar.com'>Radar</InlineLink> working remotely in <InlineLink href='https://radar.com/demo/maps#14/32.75544435227022/-97.33047972836872'>Fort Worth, TX</InlineLink>.</p>
+      <p className={styles.bioParagraph}>I&apos;m a senior engineer at <InlineLink href='https://radar.com'>Radar</InlineLink> working remotely in <InlineLink href='https://radar.com/demo/maps#13/32.754542065879974/-97.33142386594159'>Fort Worth, TX</InlineLink>.</p>
       <p className={styles.bioParagraph}>This is my medium for occasional writing and references to things I find interesting.</p>
       <div className={styles.linkSection}>
         <ArrowLink href='https://www.linkedin.com/in/bradley-schoeneweis' isExternalLink>linkedin</ArrowLink>
