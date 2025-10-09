476f0fe461a2b3bc23ad8fbbd8e7b067dc7032a9
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2025-10-08 22:21:00 -0500

    grammar fixes, add src

---
+++ code/inverted_index/src/main.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    let documents = vec![
+        "The Lord of the Rings: The Fellowship of the Ring",
+        "The Lord of the Rings: The Two Towers",
+        "The Lord of the Rings: The Return of the King",
+        "Star Wars: Episode VI - Return of the Jedi",
+    ]; 
+}
