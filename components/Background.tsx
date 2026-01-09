7eb5de726adba4348e8af4c57707541090e8b037
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2026-01-08 21:52:01 -0600

    touch some grass

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,13 +1,24 @@
+'use client'
+
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
-export const Background = () => (
-  <Image
-    alt="Background scribbles"
-    src="/images/background.svg"
-    quality={100}
-    fill
-    sizes="100vw"
-    style={{ objectFit: 'cover', zIndex: -1 }}
-  />
-);
+export const Background = () => {
+  const pathname = usePathname();
+  const isWriting = pathname.startsWith('/writing/');
+  const bgImage = isWriting ? 'background.svg' : 'background-scene.png';
+  const src = `/images/${bgImage}`;
+
+  return (
+    <Image
+      className={isWriting ? '' : "background-img"}
+      alt="Background"
+      src={src}
+      quality={100}
+      fill
+      sizes="100vw"
+      style={{ objectFit: 'cover', zIndex: -1 }}
+    />
+  );
+};
