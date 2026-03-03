a699a2baa87c3aa228ca971f0c0047a772436844
Bradley Schoeneweis <bradley.schoeneweis@gmail.com>
2026-03-03 10:45:09 -0600

    hide

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default function Home() {
         <ArrowLink href='https://github.com/bschoeneweis' isExternalLink>github</ArrowLink>
         <ArrowLink href='mailto:bradley.schoeneweis@gmail.com' isExternalLink>contact</ArrowLink>
         <ArrowLink href='/surprise'>surprise</ArrowLink>
-        <ArrowLink href='/touch-ai-grass'>touch (gen ai) grass</ArrowLink>
       </div>
     </>
   );
